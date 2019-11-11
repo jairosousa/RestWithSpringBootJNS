c467bff48b865b92296731f3c938a0d11d1b1541
Jairo Nascimento <jaironsousa@gmail.com>
2019-11-11 15:46:49 -0300

    Seção 13 - 95 - Content Negociation via YAML - YML

--- 09 RestWathSpringBootUdemyScanffold Content Negotiation/src/main/java/br/com/restapi/config/WebConfig.java
+++ 09 RestWathSpringBootUdemyScanffold Content Negotiation/src/main/java/br/com/restapi/config/WebConfig.java
@@ -1,14 +1,25 @@
 package br.com.restapi.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.com.restapi.serializatiion.converter.YamlJackson2HttpMessageConverter;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
+	
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -39,7 +50,8 @@ public class WebConfig implements WebMvcConfigurer {
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		.mediaType("xml", MediaType.APPLICATION_XML)
+		.mediaType("x-yaml", MEDIA_TYPE_YAML);
 		
 	}
 	
--- 09 RestWathSpringBootUdemyScanffold Content Negotiation/src/main/java/br/com/restapi/controller/PersonController.java
+++ 09 RestWathSpringBootUdemyScanffold Content Negotiation/src/main/java/br/com/restapi/controller/PersonController.java
@@ -23,24 +23,24 @@ public class PersonController {
 	@Autowired
 	private PersonService service;
 
-	@GetMapping(produces = {"application/json", "application/xml"})
+	@GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
 	public List<PersonVO> finAll() {
 		return service.findAll();
 	}
 
-	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml"})
+	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVO findById(@PathVariable("id") Long id) {
 		return service.findById(id);
 	}
 
-	@PostMapping(produces = {"application/json", "application/xml"},
-			consumes = {"application/json", "application/xml"})
+	@PostMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+			consumes = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 
-	@PutMapping(produces = {"application/json", "application/xml"},
-			consumes = {"application/json", "application/xml"})
+	@PutMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+			consumes = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
