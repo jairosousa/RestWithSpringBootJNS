2e83de4ffa292bb77a9e984bf3808baa25848b4c
Jairo Nascimento <jaironsousa@gmail.com>
2019-11-11 14:33:26 -0300

    Seção 12 Custom JSON Serialização

---
+++ 08 RestWathSpringBootUdemyScanffold Custom JSON/src/main/resources/db/migration/V2__Population_table_person.sql
@@ -0,0 +1,6 @@
+INSERT INTO rest_api_udemy.person (address,first_name,gender,last_name) VALUES 
+('Uberlândia - MG - Brasil','Jairo','Male','Nascimento de Sousa')
+,('Belém - PA - Brasil','Silvana','Female','Sousa')
+,('Belém - PA - Brasil','Caio','Male','Luz de Sousa')
+,('Belém - PA - Brasil','Jairo','Male','Filho')
+,('Belém - PA - Brasil','Jair','Male','Sousa');
