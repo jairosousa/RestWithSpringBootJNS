5a451d49381b269d92fe1e64c3f7dfb04bdb936c
Jairo Nascimento <jaironsousa@gmail.com>
2019-12-04 14:41:39 -0300

    Seção 19 131. Implementando Ordenação com Direction

--- 15 RestWithSpringBootUdemyScanffoldPaginacaoQueryParam/src/main/java/br/com/erudio/controller/PersonController.java
+++ 15 RestWithSpringBootUdemyScanffoldPaginacaoQueryParam/src/main/java/br/com/erudio/controller/PersonController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,10 +39,13 @@ public class PersonController {
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
 	public List<PersonVO> findAll(
 			@RequestParam(value = "page", defaultValue = "0") int page,
-			@RequestParam(value = "limit", defaultValue = "12") int limit
+			@RequestParam(value = "limit", defaultValue = "12") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction
 			) {
 		
-		Pageable pageable = PageRequest.of(page, limit);
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
 		
 		List<PersonVO> persons =  service.findAll(pageable);
 		persons
