d08b8507c03749d7141c10ac0748b5e7421cec22
Jairo Nascimento <jaironsousa@gmail.com>
2019-12-04 15:02:06 -0300

    Seção 19 132. Retornando PagedResources

--- 15 RestWithSpringBootUdemyScanffoldPaginacaoQueryParam/src/main/java/br/com/erudio/controller/PersonController.java
+++ 15 RestWithSpringBootUdemyScanffoldPaginacaoQueryParam/src/main/java/br/com/erudio/controller/PersonController.java
@@ -6,10 +6,14 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,24 +41,27 @@ public class PersonController {
 	
 	@ApiOperation(value = "Find all people" ) 
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-	public List<PersonVO> findAll(
+	public ResponseEntity<PagedResources<PersonVO>>findAll(
 			@RequestParam(value = "page", defaultValue = "0") int page,
 			@RequestParam(value = "limit", defaultValue = "12") int limit,
-			@RequestParam(value = "direction", defaultValue = "asc") String direction
+			@RequestParam(value = "direction", defaultValue = "asc") String direction,
+			PagedResourcesAssembler assembler
 			) {
 		
 		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 		
 		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
 		
-		List<PersonVO> persons =  service.findAll(pageable);
+		Page<PersonVO> persons =  service.findAll(pageable);
+		
 		persons
 			.stream()
 			.forEach(p -> p.add(
 					linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()
 				)
 			);
-		return persons;
+		
+		return new ResponseEntity<>(assembler.toResource(persons), HttpStatus.OK);
 	}	
 	
 	@ApiOperation(value = "Find a specific person by your ID" )
--- 15 RestWithSpringBootUdemyScanffoldPaginacaoQueryParam/src/main/java/br/com/erudio/services/PersonServices.java
+++ 15 RestWithSpringBootUdemyScanffoldPaginacaoQueryParam/src/main/java/br/com/erudio/services/PersonServices.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +27,15 @@ public class PersonServices {
 		return vo;
 	}
 	
-	public List<PersonVO> findAll(Pageable pageable) {
-		var entities= repository.findAll(pageable).getContent();
+	public Page<PersonVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
 		
-		return DozerConverter.parseListObjects(entities, PersonVO.class);
+		return page.map(this::convertToPerson);
 	}	
 	
+	private PersonVO convertToPerson(Person entity) {
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
 	public PersonVO findById(Long id) {
 		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
