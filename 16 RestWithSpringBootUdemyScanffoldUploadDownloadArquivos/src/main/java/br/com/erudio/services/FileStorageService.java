8a770279a3060b7d6e6e1724a5f2f35be8efa215
jaironsousa1 <jaironsousa@gmail.com>
2019-12-05 07:12:38 -0300

    Seção 20 Upload E Download Arquivos 139. Criando o Serviço Responsável por Armazenar o Arquivo em Disco

---
+++ 16 RestWithSpringBootUdemyScanffoldUploadDownloadArquivos/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -0,0 +1,62 @@
+package br.com.erudio.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.exception.FileStorageException;
+
+@Service
+public class FileStorageService {
+
+	private final Path fileStorageLocation;
+
+	@Autowired
+	public FileStorageService(FileStorageConfig fileStorageConfig) {
+		this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir())
+				.toAbsolutePath().normalize();
+		
+		/**
+		 * Metodo responsavel em criar o diretorio no construtor
+		 */
+		try {
+			Files.createDirectories(this.fileStorageLocation);
+		} catch (Exception e) {
+			throw new FileStorageException("Não foi possivel criar o diretório", e);
+		}
+
+	}
+	
+	/**
+	 * Metodo responsavel em criar o arquivo
+	 */
+	public String storageFile(MultipartFile file) {
+		String filename = StringUtils.cleanPath(file.getOriginalFilename());
+		
+		try {
+			
+			if (filename.contains("..")) {
+				throw new FileStorageException("O arquivo tem sequencia invalida " + filename);
+			}
+			
+			/**
+			 * Salvar arquivo em disco
+			 */
+			Path targetLocation = this.fileStorageLocation.resolve(filename);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			
+			
+			return  filename;
+		} catch (Exception e) {
+			throw new FileStorageException("Não foi possivel criar o arquivo " + filename + ". Por favor tente novamente", e);
+		}
+	}
+
+}
