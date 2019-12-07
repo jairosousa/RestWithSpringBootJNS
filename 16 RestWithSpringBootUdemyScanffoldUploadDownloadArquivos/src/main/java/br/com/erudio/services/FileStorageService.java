5a08c05f7d18a4b78d726f9ab6b88de6183ce342
jaironsousa1 <jaironsousa@gmail.com>
2019-12-07 19:39:46 -0300

    Seção 20 Upload E Download Arquivos 143. Implementando o Download de Arquivos

--- 16 RestWithSpringBootUdemyScanffoldUploadDownloadArquivos/src/main/java/br/com/erudio/services/FileStorageService.java
+++ 16 RestWithSpringBootUdemyScanffoldUploadDownloadArquivos/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exception.FileStorageException;
+import br.com.erudio.exception.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -52,10 +55,24 @@ public class FileStorageService {
 			Path targetLocation = this.fileStorageLocation.resolve(filename);
 			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 			
-			
 			return  filename;
+			
 		} catch (Exception e) {
 			throw new FileStorageException("Não foi possivel criar o arquivo " + filename + ". Por favor tente novamente", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String filename) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundException("Arquivo não encontrado " + filename);
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("Arquivo não encontrado " + filename, e);
 		}
 	}
 
