b69974cb5674a79dc02735e12129aaf3fe26e1b3
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-13 18:45:41 -0300

    Content Negotiation via HEADER Parameter  Changes to be committed: 	modified:   RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/config/WebConfig.java

--- RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/config/WebConfig.java
+++ RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/config/WebConfig.java
@@ -12,15 +12,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
-        .parameterName("mediaType")
-        .ignoreAcceptHeader(true)
-        .useRegisteredExtensionsOnly(false)
-        .defaultContentType(MediaType.APPLICATION_JSON)
-        .mediaType("json", MediaType.APPLICATION_JSON)
-        .mediaType("xml", MediaType.APPLICATION_XML);
+        /**
+         * Implemantação Via QUERY PARAM
+         * /api/person/v1?mediaType=xml
+         * @param configurer
+         */
+//        configurer.favorParameter(true)
+//        .parameterName("mediaType")
+//        .ignoreAcceptHeader(true)
+//        .useRegisteredExtensionsOnly(false)
+//        .defaultContentType(MediaType.APPLICATION_JSON)
+//        .mediaType("json", MediaType.APPLICATION_JSON)
+//        .mediaType("xml", MediaType.APPLICATION_XML);
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
 
     }
 }
