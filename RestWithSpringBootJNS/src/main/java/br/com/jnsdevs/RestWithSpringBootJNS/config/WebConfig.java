97a505078709771ff55cf2801c6692594d968deb
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-14 19:18:24 -0300

    Habilitando CORS de Forma Global  Changes to be committed: 	modified:   RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/config/WebConfig.java 	modified:   RestWithSpringBootJNS/src/main/resources/application.yml 	modified:   RestWithSpringBootJNS/src/test/resources/application.yml

--- RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/config/WebConfig.java
+++ RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/config/WebConfig.java
@@ -3,10 +3,12 @@ package br.com.jnsdevs.RestWithSpringBootJNS.config;
 import java.util.List;
 
 import br.com.jnsdevs.RestWithSpringBootJNS.serialization.converter.YamlJackson2HttpMesageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -14,10 +16,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
 
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
 	
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMesageConverter());
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+				//.allowedMethods("GET", "POST", "PUT")
+				.allowedMethods("*")
+				.allowedOrigins(allowedOrigins)
+				.allowCredentials(true);
 	}
 
 	@Override
