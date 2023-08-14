d99634e5d439ea9afd53f70639db7cc7d7c7f3a0
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-14 18:23:07 -0300

    Validando a Geração da Swagger UI Page com Testcontainers e Rest Assured  Changes to be committed: 	modified:   RestWithSpringBootJNS/pom.xml 	deleted:    RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/StartupTests.java 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/SwaggerIntegrationTest.java 	renamed:    RestWithSpringBootJNS/src/test/resources/application-test.yml -> RestWithSpringBootJNS/src/test/resources/application.yml

---
+++ RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/SwaggerIntegrationTest.java
@@ -0,0 +1,30 @@
+package br.com.jnsdevs.RestWithSpringBootJNS.integrationtests;
+
+import br.com.jnsdevs.RestWithSpringBootJNS.configs.TestConfigs;
+import br.com.jnsdevs.RestWithSpringBootJNS.integrationtests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+				given()
+						.basePath("/swagger-ui/index.html")
+						.port(TestConfigs.SERVER_PORT)
+						.when()
+						.get()
+						.then()
+						.statusCode(200)
+						.extract()
+						.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
