109cc857f25cd3b0e99aeb7c464ac0260109083e
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-14 17:24:41 -0300

    Criando Testes Unitários com Mockito para Validar HATEOAS em Book  Changes to be committed: 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/services/BookServicesTest.java 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/unittests/mapper/mocks/MockBook.java

---
+++ RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/unittests/mapper/mocks/MockBook.java
@@ -0,0 +1,61 @@
+package br.com.jnsdevs.RestWithSpringBootJNS.unittests.mapper.mocks;
+
+import br.com.jnsdevs.RestWithSpringBootJNS.data.vo.v1.BookVO;
+import br.com.jnsdevs.RestWithSpringBootJNS.model.Book;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 13/08/2023 - 11:34
+ */
+public class MockBook {
+
+
+    public Book mockEntity() {
+        return mockEntity(0);
+    }
+    
+    public BookVO mockVO() {
+        return mockVO(0);
+    }
+    
+    public List<Book> mockEntityList() {
+        List<Book> books = new ArrayList<Book>();
+        for (int i = 0; i < 14; i++) {
+            books.add(mockEntity(i));
+        }
+        return books;
+    }
+
+    public List<BookVO> mockVOList() {
+        List<BookVO> books = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            books.add(mockVO(i));
+        }
+        return books;
+    }
+    
+    public Book mockEntity(Integer number) {
+        Book book = new Book();
+        book.setId(number.longValue());
+        book.setAuthor("Some Author" + number);
+        book.setLaunchDate(new Date());
+        book.setPrice(25D);
+        book.setTitle("Some Title" + number);
+        return book;
+    }
+
+    public BookVO mockVO(Integer number) {
+        BookVO book = new BookVO();
+        book.setKey(number.longValue());
+        book.setAuthor("Some Author" + number);
+        book.setLaunchDate(new Date());
+        book.setPrice(25D);
+        book.setTitle("Some Title" + number);
+        return book;
+    }
+
+}
