0b612a317d1ed07c30696d50c0e4e4d3864d91f9
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-14 17:57:00 -0300

    Preparando a Infraestrutura de Testes com Testcontainers  Changes to be committed: 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/configs/TestConfigs.java 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/testcontainers/AbstractIntegrationTest.java 	new file:   RestWithSpringBootJNS/src/test/resources/application-test.yml

---
+++ RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/configs/TestConfigs.java
@@ -0,0 +1,10 @@
+package br.com.jnsdevs.RestWithSpringBootJNS.configs;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 14/08/2023 - 17:45
+ */
+public class TestConfigs {
+
+    public static final int SERVER_PORT = 8888;
+}
---
+++ RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,48 @@
+package br.com.jnsdevs.RestWithSpringBootJNS.integrationtests.testcontainers;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 14/08/2023 - 17:48
+ */
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
+
+        private static void startContainers() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
+
+        private static Map<String, String> createConnectionConfiguration() {
+            return Map.of(
+                    "spring.datasource.url", mysql.getJdbcUrl(),
+                    "spring.datasource.username", mysql.getUsername(),
+                    "spring.datasource.password", mysql.getPassword()
+            );
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource(
+                    "testcontainers",
+                    (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testcontainers);
+        }
+    }
+}
