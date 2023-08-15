38616fc8801c72341eb8461027af13d93937c350
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-15 09:22:57 -0300

    Implementando o JwtTokenFilter e o JwtConfigurer
     Changes to be committed:
    	new file:   RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/security/jwt/JwtConfigurer.java
    	new file:   RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/security/jwt/JwtTokenFilter.java

---
+++ RestWithSpringBootJNS/src/main/java/br/com/jnsdevs/RestWithSpringBootJNS/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,40 @@
+package br.com.jnsdevs.RestWithSpringBootJNS.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 15/08/2023 - 09:16
+ */
+public class JwtTokenFilter extends GenericFilterBean {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
+        if (token != null && tokenProvider.validateToken(token)) {
+            Authentication auth = tokenProvider.getAuthentication(token);
+            if (auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
