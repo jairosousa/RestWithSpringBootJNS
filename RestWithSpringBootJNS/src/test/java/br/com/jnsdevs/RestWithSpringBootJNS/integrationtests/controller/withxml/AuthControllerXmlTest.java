ac8fce24632e771ea02b708041a84c3e7d69528f
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-18 09:42:31 -0300

    Criando os Testes de Integração do Controller de Auth com XML e YML  Changes to be committed: 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/controller/withxml/AuthControllerXmlTest.java 	new file:   RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/controller/withyaml/AuthControllerYamlTest.java

---
+++ RestWithSpringBootJNS/src/test/java/br/com/jnsdevs/RestWithSpringBootJNS/integrationtests/controller/withxml/AuthControllerXmlTest.java
@@ -0,0 +1,113 @@
+package br.com.jnsdevs.RestWithSpringBootJNS.integrationtests.controller.withxml;
+
+import br.com.jnsdevs.RestWithSpringBootJNS.configs.TestConfigs;
+import br.com.jnsdevs.RestWithSpringBootJNS.data.vo.v1.security.AccountCredentialsVO;
+import br.com.jnsdevs.RestWithSpringBootJNS.data.vo.v1.security.TokenVO;
+import br.com.jnsdevs.RestWithSpringBootJNS.integrationtests.controller.withyaml.mapper.YMLMapper;
+import br.com.jnsdevs.RestWithSpringBootJNS.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 18/08/2023 - 09:24
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+public class AuthControllerXmlTest extends AbstractIntegrationTest {
+
+    private static XmlMapper objectMapper;
+    private static TokenVO tokenVO;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new XmlMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    @Test
+    @Order(1)
+    public void testSignin() throws JsonMappingException, JsonProcessingException {
+
+        AccountCredentialsVO user =
+                new AccountCredentialsVO("jairo", "admin123");
+
+        RequestSpecification specification = new RequestSpecBuilder()
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        tokenVO = given().spec(specification)
+                .config(
+                        RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig.encoderConfig()
+                                        .encodeContentTypeAs(
+                                                TestConfigs.CONTENT_TYPE_XML,
+                                                ContentType.XML)))
+                .accept(TestConfigs.CONTENT_TYPE_XML)
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenVO.class);
+
+        assertNotNull(tokenVO.getAccessToken());
+        assertNotNull(tokenVO.getRefreshToken());
+    }
+
+    @Test
+    @Order(2)
+    public void testRefresh() throws JsonMappingException, JsonProcessingException {
+
+        var newTokenVO = given()
+                .config(
+                        RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig.encoderConfig()
+                                        .encodeContentTypeAs(
+                                                TestConfigs.CONTENT_TYPE_XML,
+                                                ContentType.XML)))
+                .accept(TestConfigs.CONTENT_TYPE_XML)
+                .basePath("/auth/refresh")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
+                .pathParam("username", tokenVO.getUsername())
+                .header(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO.getRefreshToken())
+                .when()
+                .put("{username}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenVO.class);
+
+        assertNotNull(newTokenVO.getAccessToken());
+        assertNotNull(newTokenVO.getRefreshToken());
+    }
+}
